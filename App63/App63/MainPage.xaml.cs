2215c8c1ae91d2526116e51afd2b53437de6bdb2
msdnwptest <54531691+msdnwptest@users.noreply.github.com>
2020-04-29 16:50:35 +0800

    Add project files.

---
+++ App63/App63/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App63
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private bool flag;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            flag = !flag;
+
+            string pausefunctionString = @"var videos = document.querySelectorAll('video');  
+                        [].forEach.call(videos, function(video) { video.pause(); });
+                        ";
+            string playfunctionString = @"var videos = document.querySelectorAll('video');  
+                        [].forEach.call(videos, function(video) { video.play(); });
+                        ";
+            if (flag)
+            {
+                MyWebView.Eval(pausefunctionString);
+            }
+            else
+            {
+                MyWebView.Eval(playfunctionString);
+            }
+          
+
+        }
+    }
+}
